9a7e628d039b0f5d98e9d0d4557a9ea60a5f903c
The-Michael-Chen <02chenmichael@gmail.com>
2023-04-17 23:20:51 -0400

    added a trajectory

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -30,10 +30,17 @@ class PurePursuit(object):
         self.tfBuffer = tf2_ros.Buffer()
         listener = tf2_ros.TransformListener(self.tfBuffer)
         self.traj_sub = rospy.Subscriber("/trajectory/current", PoseArray, self.trajectory_callback, queue_size=1)
+        #self.load_traj_sub = rospy.Subscriber("/loaded_trajectory/path", PoseArray, self.trajectory_callback, queue_size=1)
         self.drive_pub = rospy.Publisher("/drive", AckermannDriveStamped, queue_size=1)
         self.pose_sub = rospy.Subscriber(self.odom_topic, Odometry, self.odom_callback, queue_size=1)
         self.init_pose_sub = rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, self.pose_callback, queue_size=1)
         self.dist_pub = rospy.Publisher("/dist", Float32, queue_size=1)
+    
+    # def conv_load_traj_callback(self, msg):
+    #     print("loading trajectory")
+    #     new_pose = msg.toPoseArray()
+    #     self.trajectory_callback(new_pose)
+
 
     def trajectory_callback(self, msg):
         ''' Clears the currently followed trajectory, and loads the new one from the message
